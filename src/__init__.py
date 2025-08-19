fd946a6a673b57fdc846201f5177cafb651e217a
kfwong15 <kfwong15@gmail.com>
2025-08-19 14:36:25 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,1 @@
-(cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF'
-diff --git a/src/__init__.py b/src/__init__.py
---- a/src/__init__.py
-+++ b/src/__init__.py
-@@ -0,0 +1,3 @@
-+# Package marker for src
-+
-+
-EOF
-)
+# Package marker for src
